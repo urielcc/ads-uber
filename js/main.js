fcd43bf0ab45d804efb8d450bce4addac9091a6b
Carlos De la Cruz <cdlc.lopez9411@gmail.com>
2017-11-17 21:42:42 -0800

    Agregado menu,login y registro de Pasajero

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@
       // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
       
 initMap();
+      function mostrarElemento(id){
+        //console.log(id);
+        //document.getElementById(id).show();
+        $("#"+id).show();
+      }
+
+      function ocultaElemento(id){
+        $("#"+id).hide();
+      }
+      function redirecciona(url){
+        location.href = url;
+      }
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -33.8688, lng: 151.2195},
